f3395aee6b6d5cd896fc0726415a062ecb1337ee
DCP <DCP@10.0.2.15>
2019-03-04 21:13:11 +0100

    Añadida configuración JavaDoc y comentarios

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -1,13 +1,29 @@
 package sumador;
 
+/**
+ * @see Tema10 Entornos
+ * @author DCP
+ * @version 1.0
+ * @since Febrero 2019
+ */
 public class ASumar {
 
 	String valor_inicial="";
 	
+	/**
+	 * Constructor
+	 * @param string valor_inicial es el dato que pasamos para que realice el cálculo
+	 */
 	public ASumar(String string) {
 		valor_inicial=string;
 	}
 
+	/**
+	 * @return el cálculo obtenido tras las tres condiciones reflejadas
+	 * Primera condición: que la cadena sea un solo carácter
+	 * Segunda condición: que la cadena sea un número negativo
+	 * Tercera condición: que la cadena sea un número que no cumpla las dos condiciones anteriores
+	 */
 	public String mostrar() {
 		String numero = valor_inicial;
         if (numero.length() == 1) {
