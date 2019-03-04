336248a586a9887132dbba4cdb5d0b092c0da6cf
DCP <DCP@10.0.2.15>
2019-03-04 19:48:31 +0100

    Primera prueba con éxito

--- src/sumador/ASumar.java
+++ src/sumador/ASumar.java
@@ -2,4 +2,18 @@ package sumador;
 
 public class ASumar {
 
+	String valor_inicial="";
+	
+	public ASumar(String string) {
+		valor_inicial=string;
+	}
+
+	public String mostrar() {
+		String numero = valor_inicial;
+        if (numero.length() == 1) {
+            numero =numero + " = " + numero;
+        }
+        return numero;
+	}
+
 }
